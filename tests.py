e1208cc99c2bde6b5ebd742c37743a4744b56e84
aikbix <ian@aikbix.com>
2013-09-07 17:48:59 -0700

    stuff

---
+++ tests.py
@@ -0,0 +1,187 @@
+import things
+import unittest
+from queue import Queue
+from random import randint
+
+
+class TestActorEventLoop(unittest.TestCase):
+    def test_single_item_in_queue(self):
+        '''
+        Create an actor, start it. Send a message to it and verify that it
+        recieved and handled the message successfully.
+        '''
+        # use a queue
+        queue = Queue()
+        message = "hello world"
+
+        class Actor(things.Actor):
+            def on_message(self, data):
+                queue.put(data)
+
+        actor = Actor()
+        actor.put(message)
+        result = queue.get(timeout=5)
+
+        assert result == message
+
+    def test_multiple_items_in_queue(self):
+        queue = Queue()
+        messages = list(range(100))
+
+        class Actor(things.Actor):
+            def on_message(self, data):
+                queue.put(data)
+
+        actor = Actor()
+        for message in messages:
+            actor.put(message)
+            result = queue.get(timeout=5)
+
+            assert result == message
+
+            if result == messages[-1]:
+                break
+
+
+class TestActorCommunication(unittest.TestCase):
+    def test_send(self):
+        '''
+        Send 1000 messages from one actor to another.
+        '''
+
+        queue = Queue()
+
+        class FirstActor(things.Actor):
+            def on_message(self, data):
+                self.send(data['actor'], data['message'])
+
+        class SecondActor(things.Actor):
+            def on_message(self, data):
+                queue.put(data)
+
+        first_actor = FirstActor()
+        second_actor = SecondActor()
+
+        for n in range(1000):
+            first_actor.put({
+                'actor': second_actor,
+                'message': n
+            })
+
+            result = queue.get(timeout=5)
+
+            assert result == n
+
+    def test_call(self):
+        '''
+        Pretty much a copy of self.test_send, except we use Actor.call instead
+        of Actor.send.
+        '''
+
+        queue = Queue()
+
+        class FirstActor(things.Actor):
+            def on_message(self, data):
+                self.call(data['actor'], data['message'])
+
+        class SecondActor(things.Actor):
+            def on_message(self, data):
+                queue.put(data)
+
+        first_actor = FirstActor()
+        second_actor = SecondActor()
+
+        for n in range(1000):
+            first_actor.put({
+                'actor': second_actor,
+                'message': n
+            })
+
+            result = queue.get(timeout=5)
+
+            assert result == n
+
+    def test_listen(self):
+        '''
+        Generate 100 actors and link them together on a one-dimensional path.
+        Pass 1000 messages from the starting point and check to make sure they
+        reach the end point properly.
+        '''
+
+        queue = Queue()
+        actors = []
+
+        class Actor(things.Actor):
+            def on_message(self, data):
+                self.broadcast(data)
+
+        class Endpoint(things.Actor):
+            def on_message(self, data):
+                queue.put(data)
+
+        for n in range(randint(1, 100)):
+            actor = Actor()
+            actors.append(actor)
+
+            if n == 0:
+                continue
+            else:
+                actor.listen(actors[n - 1])
+
+        endpoint = Endpoint()
+        endpoint.listen(actors[-1])
+        actors.append(endpoint)
+
+        for data in range(1000):
+            actors[0].put(data)
+            result = queue.get(timeout=5)
+            assert result == data
+
+    def test_broadcast(self):
+        '''
+        Generate a tree structure of actors, broadcast 100 messages from the
+        root of the tree and verify that the data reached the leafs.
+        '''
+
+        queue = Queue()
+
+        class Actor(things.Actor):
+            def on_message(self, data):
+                self.broadcast(data)
+
+        class Leaf(things.Actor):
+            def on_message(self, data):
+                queue.put(data)
+
+        # Create actors
+        root = Actor()
+        level_1 = [Actor() for i in range(2)]
+        level_2 = [Actor() for i in range(4)]
+        level_3 = [Leaf() for i in range(8)]
+
+        child = 0
+        parent = 1
+        tree_range = lambda n: enumerate(sorted(list(range(n))
+                                                + list(range(n)))[:n])
+
+        # Link them together
+        # TODO Clean this up a bit
+        for n in tree_range(8):
+            level_3[n[child]].listen(level_2[n[parent]])
+
+        for n in tree_range(4):
+            level_2[n[child]].listen(level_1[n[parent]])
+
+        for n in range(2):
+            level_1[n].listen(root)
+
+        for data in range(1000):
+            root.put(data)
+
+            for n in range(len(level_3)):
+                result = queue.get(timeout=5)
+                assert result == data
+
+
+if __name__ == '__main__':
+    unittest.main()
--- things.py
+++ things.py
@@ -1,26 +1,25 @@
-from collections import namedtuple
-from concurrent.futures import ThreadPoolExecutor
+"""
+TODO synchronous behavior for non-actor calls.
+"""
+
+from greenlet import greenlet
 from functools import wraps
 from queue import Queue
-from threading import Thread, Event
+from threading import Thread
 from uuid import uuid1
-
-# TODO executor for greenlets
-
-# TODO Shape or args/kwargs?
-message = namedtuple('message', (
-    'data',
-    'return_address',
-    'event_id'
-))
+from functools import partial
 
 
-def create_message(data, return_address=None, event_id=None):
-    '''
-    Generates a message that can be passed to an actor.
-    '''
+###############################################################################
+# Configuration
+###############################################################################
 
-    return message(data, return_address, event_id)
+
+DAEMON_THREADS = True
+
+
+if DAEMON_THREADS:
+    Thread = partial(Thread, daemon=True)
 
 
 def lazy_property(fn):
@@ -41,10 +40,70 @@ def lazy_property(fn):
     return getter
 
 
+###############################################################################
+# Message data structures and methods
+###############################################################################
+
+
+#
+# Address messages.
+#
+
+
+# Data that will be transported to an actor
+MESSAGE_DATA = 0
+
+# Where to send return data if requested to do so.
+MESSAGE_RETURN_ACTOR = 1
+
+# The context of the calling microthread within the calling actor.
+MESSAGE_CURRENT_CONTEXT = 2
+
+# A microthread that needs to be resumed when recieved from the queue.
+MESSAGE_RESUME_CONTEXT = 3
+
+
+def create_message(data, return_actor=None, current_context=None,
+                   resume_context=None):
+    '''
+    Generates a message that can be passed to an actor.
+    '''
+
+    return (data, return_actor, current_context, resume_context)
+
+
+#
+# Microthread messages.
+#
+
+
+# Is the greenlet waiting for a result?
+RESULT_WAITING = 0
+
+# Data that's being returned
+RESULT_DATA = 1
+
+
+def create_result(waiting, data):
+    '''
+    This represents the data structure passed between greenlets.
+    '''
+
+    return (waiting, data)
+
+
+###############################################################################
+# Actor classes
+###############################################################################
+
+
 class Actor(object):
     '''
     A variation of the actor model in Python.
     See: http://en.wikipedia.org/wiki/Actor_model
+
+    TODO Should public methods represent the actor and private methods
+         represent an external actor?
 
     Abstract methods
     ----------------
@@ -54,19 +113,125 @@ class Actor(object):
     --------------
         subscribe
         broadcast
-        address
         put
+
+    Internal methods
+    ----------------
+        send
+        call
     '''
 
-    def __init__(self, max_workers=4):
+    def __init__(self, max_workers=4, subscribed_to=[], spawn_as=Thread):
         self.max_workers = max_workers
-        self.return_addresses = {}
-        # A list of addresses for actors subscribed to this actor.
+        self.return_actores = {}
+
+        # Add subscribers if listed
+        for actor in subscribed_to:
+            self.listen(actor)
+
+        # A list of actors subscribed to this actor.
         # TODO Use bisect to turn subscribers into binary search trees?
         self.subscribers = []
-        # TODO make main modular so we can use greenlets and processes
-        self.__main = Thread(target=self._event_loop, daemon=True)
+        # TODO use an executor instead.
+        # TODO create a load balancing queue instead.
+        self.__main = spawn_as(target=self._event_loop)
         self.__main.start()
+
+    def __rshift__(self, data):
+        self.put(data)
+
+    def __ilshift__(self, data):
+        '''
+        TODO use current greenlet.
+        '''
+
+        pass
+
+    def _event_loop(self):
+        '''
+        Starts the event loop method as a greenlet.
+        '''
+
+        self.__event_loop_context = greenlet(self.__start_event_loop)
+        self.__event_loop_context.switch()
+
+    def __start_event_loop(self):
+        '''
+        Microthreaded event loop. The loop waits for a message to get put in
+        the queue. Upon recieving a message, it checks to see if it's a return
+        value from a call statement in an existing microthread or a new
+        message. After retrieving the return value from that microthread, the
+        initial message is checked for the presence of a return actor. If a
+        return actor and corresponding microthread exist, then push it onto
+        the queue of the calling actor. The loop is then resumed.
+
+        TODO clean this up a bit, separate the methods out maybe?
+        '''
+
+        while 1:
+            # Wait for an item on the queue.
+            message = self._queue.get()
+            switch_data = message[MESSAGE_DATA]
+            resume_context = message[MESSAGE_RESUME_CONTEXT]
+            return_actor = message[MESSAGE_RETURN_ACTOR]
+            current_context = message[MESSAGE_CURRENT_CONTEXT]
+
+            # Set the context to an existing microthread if one was passed in,
+            # otherwise create a new one.
+            if resume_context:
+                context = resume_context
+                switch_data = message[MESSAGE_DATA]
+            else:
+                context = self._create_microthread(self.handle_message)
+                switch_data = message
+
+            # Enter the context's frame and retrieve a result.
+            self.__current_frame = context
+            result = context.switch(switch_data)
+
+            # Determine if the microthread is waiting for a result
+            waiting = result[RESULT_WAITING]
+            # Get the output of the microthread.
+            data = result[RESULT_DATA]
+
+            # Send a message to the return actor if the message was recieved
+            # from another actor.
+            if return_actor and current_context and not waiting:
+                message = create_message(data, resume_context=current_context)
+                return_actor._put(message)
+
+            self.__current_frame = None
+
+    def _create_microthread(self, target):
+        microthread = greenlet(target)
+        microthread.actor = self
+
+        return microthread
+
+    def call(self, actor, data):
+        '''
+        Puts a message in another actor's mailbox with a corresponding return
+        actor and reference to the current microthread. The microthread is
+        then switched off and execution returns to the event loop until a
+        result comes back. Upon recieving the result, the microthread is
+        switched back on and the value is returned to whatever called this
+        method.
+
+        TODO what about external calls from non-actors, probably should
+             generate a queue and block with a timeout until we recieve a
+             response
+        TODO should call reflect this actor instead of an external actor?
+        TODO use current greenlet instead of data being passed in, handle
+             non-actor calls.
+        '''
+
+        message = create_message(data, return_actor=self,
+                                 current_context=self.__current_frame)
+        actor._put(message)
+        context_result = create_result(True, None)
+        result = self.__event_loop_context.switch(context_result)
+
+        return result
 
     def on_message(self, data):
         '''
@@ -76,58 +241,64 @@ class Actor(object):
 
         pass
 
-    def subscribe(self, address):
+    def subscribe(self, actor):
         '''
-        Have an address subscribe to this actor.
+        Subscribe to this actor.
         '''
 
-        self.subscribers.append(address)
+        if actor not in self.subscribers:
+            self.subscribers.append(actor)
+
+    def listen(self, actor):
+        '''
+        Listen to another actor.
+        '''
+
+        if self not in actor.subscribers:
+            actor.subscribers.append(self)
 
     def broadcast(self, data):
         '''
-        Send a message to all addresses subscribed to this actor.
+        Send a message to all actor subscribed to this actor.
         '''
 
         for subscriber in self.subscribers:
-            subscriber.put(data)
+            subscriber._put(create_message(data))
 
     def handle_message(self, message):
         '''
-        Handles recieving a message and returning a response, this is called
-        before on_message.
+        Handles recieving a raw message and returning a response, this is
+        called before on_message.
         '''
+
+        result = self.on_message(message[MESSAGE_DATA])
 
         # TODO handle errors
-        result = self.on_message(message.data)
-
-        if message.return_address and message.event_id:
-            message.return_address.handle_response(result, message)
-
-    def _event_loop(self):
-        '''
-        Main event loop for this actor.
-        '''
-
-        with self._executor(max_workers=self.max_workers) as e:
-            while 1:
-                task = self._queue.get()
-                e.submit(self.handle_message, task)
+        return create_result(False, result)
 
     def put(self, message):
         '''
         Put a message in this actor's mailbox.
         '''
 
+        self._put(create_message(message))
+
+    def _put(self, message):
+        '''
+        Put a message in this actor's mailbox, do not generate a generic
+        message data structure.
+        '''
+
         # TODO handle errors for this
-        self._queue.put(create_message(message))
+        # TODO should this create a message or recieve raw data? Both perhaps?
+        self._queue.put(message)
 
-    @lazy_property
-    def address(self):
+    def send(self, actor, data):
         '''
-        Address for this actor.
+        Put a message in a specified actor's mailbox.
         '''
 
-        return Address(self)
+        actor._put(create_message(data))
 
     @lazy_property
     def _queue(self):
@@ -137,28 +308,30 @@ class Actor(object):
 
         return Queue()
 
-    @property
-    def _executor(self):
+
+class BusHandler(object):
+    def __init__(self, parent):
+        self.parent = parent
+
+    def put(self, data):
+        pass
+
+    def call(self, data):
+        pass
+
+    def __lshift__(self, other):
         '''
-        Executor object.
-        TODO remove
+        Sugar syntax for putting a message.
         '''
 
-        return ThreadPoolExecutor
+        self.put(other)
 
+    def __ilshift__(self, other):
+        '''
+        Sugar syntax for call.
+        '''
 
-class RemoteActor(Actor):
-    '''
-    Implements actor behavior for actors that do not exist within the current
-    program (eg. socket, memory address, etc)
-    '''
-
-    def __init__(self, transport_layer, *args, **kwargs):
-        self.transport_layer = transport_layer
-        super().__init__(*args, **kwargs)
-
-    def on_message(self, data):
-        self.transport_layer.handle_send(data)
+        pass
 
 
 class Bus(Actor):
@@ -166,12 +339,30 @@ class Bus(Actor):
     An actor which emulates bus behavior with its method names. Any method
     subclassed with Bus will handle recieving a message addressed to that name.
 
+    Put symantics
+    -------------
+
+    Bus.publish (<name of handler>, <data>)
+    Bus.<handler>.put(data)
+    Bus.<handler> << data
+
+    Call symantics
+    --------------
+
+    Bus.<handler>.call(data)
+    Bus.<handler> <<= data
+
     Public methods
     --------------
         on_invalid_call
+
+    TODO You need some way to get all methods
     '''
 
     def on_message(self, data):
+        pass
+
+    def publish(self, name, data):
         pass
 
     def on_invalid_call(self, data):
@@ -179,128 +370,3 @@ class Bus(Actor):
 
     def handle_message(self, data):
         self.on_message(data)
-
-
-class Address(object):
-    '''
-    Points to an actor.
-
-    Public methods
-    --------------
-        call
-        handle_response
-        put
-        subscribe
-    '''
-
-    def __init__(self, actor, key=None):
-        self.actor = actor
-        self.key = key
-        self.events = {}
-        self.results = {}
-
-    def call(self, data, timeout=None):
-        '''
-        THIS WILL BLOCK
-        ---------------
-
-        Put a message in the actor's mailbox and wait until a response is
-        recieved or times out. This is used to emulate synchronous syntax.
-
-        TODO
-        ----
-
-        Remove the fucking ugly thread blocking bullshit. What if we called
-        handle_message directly? There's no real reason to use up another
-        thread, we could just branch the frame into another actor. There would
-        still need to be some form of blocking or callback thingy if we want to
-        wait for responses from multiple actors. Does that require an entire
-        method on its own?
-
-        However, there's also switching and cooperative multitasking. This
-        would make the application single-threaded, unless we did magic with
-        the frame of the actor.
-
-        '''
-
-        # Generate unique identifier for the return signature
-        event_id = uuid1().int
-        event = Event()
-        message = create_message(data, self, event_id)
-
-        self.events[event_id] = event
-        self.actor.put(message)
-        # Block until a response is recieved, or it times out.
-        event.wait(timeout)
-
-        # TODO handle timeouts
-
-        result = self.results.get(event_id)
-
-        if result:
-            del self.results[event_id]
-
-        del self.events[event_id]
-
-        return result
-
-    def handle_response(self, result, original_message):
-        '''
-        Handles triggering the response event in the calling thread.
-
-        Which is more clear, putting this method in Address or Actor?
-        '''
-
-        event_id = original_message.event_id
-        event = self.events.get(event_id)
-
-        if event:
-            event.set()
-        else:
-            # TODO error handling
-            pass
-
-    def put(self, data):
-        '''
-        Puts a message in the addressed actor's mailbox.
-        '''
-
-        self.actor.put(data)
-
-    def subscribe(self, actor):
-        '''
-        Subscribes an actor (subject) to the addressed actor. When the
-        addressed actor's broadcast() method is called, the subject will
-        recieve the data.
-        '''
-
-        self.actor.subscribe(actor.address)
-
-
-class TransportLayer(Actor):
-    '''
-    Defines behavior for transporting data between a local and remote actor.
-    TODO use async socket.
-
-    Abstract methods
-    ----------------
-        handle_send
-        handle_recieve
-        serialize
-        deserialize
-    '''
-
-    def __init__(self, *args, **kwargs):
-        pass
-
-    def handle_send(self, data):
-        pass
-
-    def handle_recieve(self, data):
-        pass
-
-    def serialize(self, data):
-        pass
-
-    def deserialize(self, data):
-        pass
